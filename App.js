817b1969ca0ff3a912829c9ff93664438920fd6a
Nithya1625 <70477925+Nithya1625@users.noreply.github.com>
2022-08-09 10:53:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PickImage from "./Screens/camera.js";
+
+export default class App extends React.Component {
+  render(){
+  return (
+    <PickImage/>
+  );}
+}
+
